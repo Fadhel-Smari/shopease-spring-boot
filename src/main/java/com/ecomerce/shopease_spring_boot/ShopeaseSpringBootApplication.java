9d95749183052ba2bfc1b89fc8e646a796880f97
Fadhel-Smari <smari.fadhel@courrier.uqam.ca>
2025-06-02 20:28:53 -0400

    Initial commit - Spring Boot project initialized

---
+++ src/main/java/com/ecomerce/shopease_spring_boot/ShopeaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.shopease_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeaseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomerce/shopease_spring_boot/ShopeaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.shopease_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeaseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
